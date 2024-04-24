0e5a824307d7fabe52eceaacc7f929f4d3fdd3da
GitHub Action <action@github.com>
2024-04-24 15:17:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/yinchangbo/ycbideaplugin01/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/yinchangbo/ycbideaplugin01/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.yinchangbo.ycbideaplugin01.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.yinchangbo.ycbideaplugin01.MyBundle
+import com.github.yinchangbo.ycbideaplugin01.services.MyProjectService
 import javax.swing.JButton
 
 
